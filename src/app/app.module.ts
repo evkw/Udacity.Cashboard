cc9b112cb852ba854579d5b3aa4828b149e026c4
Evan Wallace <evanwallace@Evans-MacBook-Pro.local>
2016-09-20 12:43:46 +1000

    working scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,40 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MdCoreModule } from '@angular2-material/core';
+import { MdProgressCircleModule } from '@angular2-material/progress-circle';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdListModule } from '@angular2-material/list';
+import { MdIconModule } from '@angular2-material/icon';
+
 import { AppComponent } from './app.component';
 import { AssetTrackerComponent } from './asset-tracker/asset-tracker.component';
 import { IssueAdminComponent } from './issue-admin/issue-admin.component';
 import { IssuesComponent } from './issues/issues.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     AssetTrackerComponent,
     IssueAdminComponent,
-    IssuesComponent
+    IssuesComponent,
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    routing,
+
+    MdCoreModule,
+    MdProgressCircleModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdListModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
